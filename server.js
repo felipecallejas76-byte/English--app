64b878a06962434fac7d83a00db05880e37b4b31
felipecallejas76-byte <felipecallejas76@gmail.com>
2026-03-31 18:25:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import path from "path";
 import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import express from "express";
 import cors from "cors";
 import fetch from "node-fetch";
