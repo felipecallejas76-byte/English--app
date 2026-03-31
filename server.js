1fa41fa25af067ef7013d784eb94a7beb75be288
felipecallejas76-byte <felipecallejas76@gmail.com>
2026-03-31 21:31:01 +0200

    Fix syntax error in server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import path from "path";
 import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url));
 const __dirname = path.dirname(__filename);
 import express from "express";
 import cors from "cors";
